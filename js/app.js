ddbcfb1ead3f11b36dfcc9c792da412a7d1c68bb
poohVini <antonovdiv@gmail.com>
2021-11-24 10:35:14 +0300

    dsfsdf

--- js/app.js
+++ js/app.js
@@ -394,6 +394,37 @@ window.onload = function () {
 	}
 };
 
+"use strict"
+
+
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	Mobile: function () {
+		return navigator.userAgent.search(/mobile/i);
+	},
+	any: function () {
+		return ((isMobile.Android() || isMobile.BlackBerry()) || (isMobile.iOS() || isMobile.Opera()) || (isMobile.Windows() || isMobile.Mobile() > 0));
+	}
+}
+if (isMobile.any()) {
+	document.body.classList.add("_touch");
+} else {
+	document.body.classList.add("_pc");
+}
 
 
 
